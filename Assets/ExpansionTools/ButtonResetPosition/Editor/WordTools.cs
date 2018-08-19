200a0fcf114a574b8d68b1c4f71f8935115d897c
Himakuma(kumatta_ss) <ssapp.devuser@gmail.com>
2018-08-19 20:01:59 +0900

    ALLメニュー追加

--- Assets/ExpansionTools/ButtonResetPosition/Editor/WordTools.cs
+++ Assets/ExpansionTools/ButtonResetPosition/Editor/WordTools.cs
@@ -8,14 +8,25 @@ using VRCSDK2;
 public class WordTools : EditorWindow
 {
     [MenuItem("VRChat Word/Button Positionrest")]
-    private static void Create()
+    private static void AllResetSetting()
+    {
+        Button resetButton = GetResetButton();
+        if (resetButton != null)
+        {
+            VRC_SceneResetPosition[] sceneResetPositions = resetButton.transform.parent.gameObject.GetComponentsInChildren<VRC_SceneResetPosition>();
+            AddResetOnClickListener(resetButton, sceneResetPositions);
+        }
+    }
+
+
+    [MenuItem("VRChat Word/（ALL）Button Positionrest")]
+    private static void ResetSetting()
     {
         Button resetButton = GetResetButton();
         if (resetButton != null)
         {
 
 
-            /**
             GameObject[] findResultGameObjects = (GameObject[])FindObjectsOfType(typeof(GameObject));
             List<GameObject> gameObjectList = new List<GameObject>();
             gameObjectList.AddRange(findResultGameObjects);
@@ -23,26 +34,23 @@ public class WordTools : EditorWindow
             foreach (GameObject gameObj in gameObjectList)
             {
                 VRC_SceneResetPosition[] sceneResetPositions = gameObj.GetComponents<VRC_SceneResetPosition>();
-            */
-                VRC_SceneResetPosition[] sceneResetPositions = resetButton.transform.parent.gameObject.GetComponentsInChildren<VRC_SceneResetPosition>();
-                if (sceneResetPositions != null)
-                {
-                    GameObject resetObj = resetButton.gameObject;
-                    foreach (VRC_SceneResetPosition sceneResetPosition in sceneResetPositions)
-                    {
-//                        EventTrigger eventTrigger = resetObj.AddComponent<EventTrigger>();
-//                        UnityAction action = new UnityAction();
-                        UnityEventTools.RemovePersistentListener(resetButton.onClick, sceneResetPosition.ResetPosition);
-                        UnityEventTools.AddPersistentListener(resetButton.onClick, sceneResetPosition.ResetPosition);
-                        // resetButton.onClick.AddListener(sceneResetPosition.ResetPosition);
-//                        VRC_SceneResetPosition[] sceneResetPositions = gameObj.GetComponents<VRC_SceneResetPosition>();
-//                        { UnityEngine.Events.PersistentCall}
-                    }
-                }
-//            }
+                AddResetOnClickListener(resetButton, sceneResetPositions);
+            }
         }
     }
 
+    private static void AddResetOnClickListener(Button resetButton, VRC_SceneResetPosition[] sceneResetPositions)
+    {
+        if (sceneResetPositions != null)
+        {
+            GameObject resetObj = resetButton.gameObject;
+            foreach (VRC_SceneResetPosition sceneResetPosition in sceneResetPositions)
+            {
+                UnityEventTools.RemovePersistentListener(resetButton.onClick, sceneResetPosition.ResetPosition);
+                UnityEventTools.AddPersistentListener(resetButton.onClick, sceneResetPosition.ResetPosition);
+            }
+        }
+    }
 
 
     private static Button GetResetButton()
